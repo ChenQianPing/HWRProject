b892d739109515f3f3cfb9f13d664680a5eac2b1
ChenQianPing <chenqianping@chenqianpingdemacbook-air.local>
2016-04-27 16:33:21 +0800

    Initial Commit

---
+++ HWRProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HWRProject
+//
+//  Created by ChenQianPing on 16/4/27.
+//  Copyright © 2016年 ChenQianPing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
